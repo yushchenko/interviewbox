3584e6c8a7c8731f16cc5c16c0f45d2aef71d127
Valery Yushchenko <valery.yushchenko@gmail.com>
2012-06-23 11:58:50 +1000

    parsing hash and sending join request to server

--- server/app.js
+++ server/app.js
@@ -3,13 +3,16 @@ var port = 8081,
 
 io.sockets.on('connection', function (socket) {
 
+
+    socket.on('join', function(joinRequest, callback) {
+        
+        console.log(joinRequest);
+
+        callback('interview');
+    });
+
     socket.on('disconnect', function() {
         console.log('user disconnected');
     });
-
-    socket.on('publish', function(data) {
-        console.log(data);
-    });
-
-    socket.emit('welcome', 'hi here!');
+    
 });
